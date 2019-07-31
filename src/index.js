ad4a6e81bf5df0d7ad31c952a556c41ebcda30fd
Leo <leodesouzauy@hotmail.com>
2019-07-31 04:59:07 -0300

    road to Audit 100x4 (100x3)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-//import ThisWeather from './ThisWeather';
+import ThisWeather from './ThisWeather';
 import './sass/main.css';
 
 import('./js/customConsole').then(module => module.fcy('Welcome to this.weather!'));
 
-import(/* webpackChunkName: "this.weather" */ './ThisWeather')
-  .then(module => render(<module.default />, document.getElementById('root')));
+render(<ThisWeather />, document.getElementById('root'));
